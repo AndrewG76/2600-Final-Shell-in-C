48f905fd6ec9d64b675c79c8db3f94e894887555
AndrewG76 <aggravino@cpp.edu>
2021-12-02 17:00:49 +0000

    Finish main method

--- lsh.c
+++ lsh.c
@@ -1,1 +1,15 @@
-//Test
+//Gravino, Andrew
+//CS2600, Final Part 1
+//12-10-21
+
+int main(int argc, char **argv)
+{
+    //Load config files, if any
+
+    //Run command loop
+    lsh_loop();
+
+    //Perform any shutdown/cleanup
+    return EXIT_SUCCESS;
+}
+
